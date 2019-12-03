f42ba2b857b0a02ffda6b5c0932c44113f97bd02
john anisere <johnanisere@gmail.com>
2019-12-03 10:15:10 +0100

    fix

--- .storybook/config.js
+++ .storybook/config.js
@@ -12,7 +12,7 @@ addDecorator((storyFn, context) => withConsole()(storyFn)(context));
 // automatically import all files ending in *.stories.js
 configure(
   [
-    require.context("../src/components", true, /\.stories\.js$/),
+    require.context("../src", true, /\.stories\.js$/),
     require.context("../stories", true, /\.stories\.js$/)
   ],
   module
