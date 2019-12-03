c9544615d783a1b242bfd2b1104ae493c5a8ef82
Adams Temidire A <gr8temi@gmail.com>
2019-12-03 15:32:19 +0100

    remove redundant codes

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,26 +1,5 @@
 import * as colors from "../components/Colors/color";
 const theme = {
-  colors: {
-    primaryLight: colors.primaryLight,
-    primary: colors.primary,
-    primaryDark: colors.primaryDark,
-    primaryDarker: colors.primaryDarker,
-    secondary: colors.secondary,
-    secondaryLighter: colors.secondaryLighter,
-    secondaryLight: colors.secondaryLight,
-    secondaryDark: colors.secondaryDark,
-    secondaryDarker: colors.secondaryDarker,
-    warning: colors.warning,
-    warningDark: colors.warningDark,
-    warningDarker: colors.warningDarker,
-    success: colors.success,
-    successDark: colors.successDark,
-    successDarker: colors.successDarker,
-    blueDark: colors.blueDark,
-    danger: colors.danger,
-    dangerDark: colors.dangerDark,
-    dangerDarker: colors.dangerDarker
-  },
- 
+  colors,
 };
 export default theme;
