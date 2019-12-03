a2e8019d061246f3efc7cc407e66f5d60f6f805a
john anisere <johnanisere@gmail.com>
2019-12-03 16:10:28 +0100

    fix

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,18 @@
 import React from "react";
-import Styled,{ThemeProvider} from 'styled-components'
-import theme from '../components/theme'
-import backgroundColor, { color } from "../components/Colors/mixins";
+import Styled, { ThemeProvider } from "styled-components";
+import theme from "../components/theme";
+import backgroundColor, { color } from "../components/color/mixins";
 
 let H1 = Styled.h1`
     ${backgroundColor}
     ${color}
-`
-const App = () =>
-<ThemeProvider theme={theme}>
-    <H1 primary successColor>Welcome</H1>
-</ThemeProvider>;
+`;
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <H1 primary successColor>
+      Welcome
+    </H1>
+  </ThemeProvider>
+);
 
 export default App;
