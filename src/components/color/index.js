dae66597f05805ca3fb1b37eb88d48c322984e68
john anisere <johnanisere@gmail.com>
2019-12-03 16:08:35 +0100

    fix

--- src/components/color/index.js
+++ src/components/color/index.js
@@ -1,26 +1,20 @@
-import Styled from "styled-components"
-import React from 'react'
-import backgroundColor,{color} from "./mixins"
+import Styled from "styled-components";
+import React from "react";
+import backgroundColor, { color } from "./mixins";
 
 let Defin = Styled.div`
-    width:"30px";
     ${backgroundColor}
     ${color}
 `;
 /**
- * This component renders color guide for the application 
+ * This component renders color guide for the application
  * @param {string} background This is the background color of the component
  * @param {string} label This is the description of the component
  * @param {string} color This determines the text color of the component
  */
 
-let Color = ({label,...props})=>{
-    
-   return (
-        <>
-        <Defin  {...props}>{label}</Defin>
-        </>
-
-)}
+let Color = ({ label, ...props }) => {
+  return <Defin {...props}>{label}</Defin>;
+};
 
 export default Color;
