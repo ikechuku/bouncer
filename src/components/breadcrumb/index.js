00e6e43c733c8a4bcc44124b0c212f11ebf8274b
Adams Temidire A <gr8temi@gmail.com>
2019-12-05 14:58:20 +0100

    add comments to breadcrumb

--- src/components/breadcrumb/index.js
+++ src/components/breadcrumb/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Styled from 'styled-components'
+// imports mixins for setting background color and text color
 import BackgroundColor, {color} from '../color/mixins'
 
 const Crumb = Styled.div`
@@ -11,7 +12,7 @@ const Crumb = Styled.div`
     justify-content:center;
     align-items:center;
 `
-
+// Breadcrumb component implementation
 function Breadcrumb({children,...props}) {
     return(
     <Crumb {...props}>{children}</Crumb>
