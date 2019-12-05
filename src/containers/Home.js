6ca196b054f0f1fda4144340e4c862e14ea65d0c
Adams Temidire A <gr8temi@gmail.com>
2019-12-05 15:41:40 +0100

    home changes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Styled, { ThemeProvider } from "styled-components";
+
 import theme from "../components/theme";
 import backgroundColor, { color } from "../components/color/mixins";
 import Button from "../components/button";
@@ -15,8 +16,8 @@ let H1 = Styled.h1`
 const App = () => (
 
   <ThemeProvider theme={theme}>
-    <br />
-    <H1 danger successColor>
+    <Header secondaryLight xSmall />
+    <H1 primary successColor>
       Welcome
     </H1>
     <Button btnInverseDanger btnLarge>
