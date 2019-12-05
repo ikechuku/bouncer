ee63781f5085383bd45858a1f4e73da999ae6a59
Adams Temidire A <gr8temi@gmail.com>
2019-12-05 16:39:42 +0100

    add text properties

--- stories/Breadcrumb.stories.js
+++ stories/Breadcrumb.stories.js
@@ -1,48 +1,218 @@
-import React from 'react';
-import { ThemeProvider } from "styled-components";
+import React from "react";
+import Styled, { ThemeProvider } from "styled-components";
 import theme from "../src/components/theme";
-import {Link} from 'react-router-dom'
-import Breadcrumb from '../src/components/breadcrumb';
-import { withKnobs } from '@storybook/addon-knobs';
+import Breadcrumb from "../src/components/breadcrumb";
+import { withKnobs } from "@storybook/addon-knobs";
+import { color } from "../src/components/color/mixins";
+import { fontSize } from "../src/components/text/mixins";
+const P = Styled.p`
+    ${color}
+    ${fontSize}
+    padding:0 0.3rem 0 0.3rem;
+`;
 
-export const primary = ()=>(
-    <ThemeProvider theme={theme}>
-        <Breadcrumb primaryLight secondaryLightColor>Home/product </Breadcrumb><br/>
-        <Breadcrumb primary secondaryLightColor>Home/product </Breadcrumb><br/>
-        <Breadcrumb primaryDark secondaryLightColor>Home/product </Breadcrumb><br/>
-        <Breadcrumb primaryDarker secondaryLightColor>Home/product </Breadcrumb>
-    </ThemeProvider>
-)
-export const danger = ()=>(
-    <ThemeProvider theme={theme}>
-        <Breadcrumb danger secondaryLightColor>Home/product</Breadcrumb><br/>
-        <Breadcrumb dangerDark secondaryLightColor>Home/product</Breadcrumb><br/>
-        <Breadcrumb dangerDarker secondaryLightColor>Home/product</Breadcrumb>
-    </ThemeProvider>
-)
-export const secondary = ()=>(
-    <ThemeProvider theme={theme}>
-        <Breadcrumb secondary primaryColor>Home/product</Breadcrumb><br/>
-        <Breadcrumb secondaryDark primaryColor>Home/product</Breadcrumb><br/>
-        <Breadcrumb secondaryDarker secondaryLightColor>Home/product</Breadcrumb>
-    </ThemeProvider>
-)
-export const warning = ()=>(
-    <ThemeProvider theme={theme}>
-        <Breadcrumb warning >Home/product</Breadcrumb><br/>
-        <Breadcrumb warningDark >Home/product</Breadcrumb><br/>
-        <Breadcrumb warningDarker >Home/product</Breadcrumb>
-    </ThemeProvider>
-)
-export const success = ()=>(
-    <ThemeProvider theme={theme}>
-        <Breadcrumb success >Home/product</Breadcrumb><br/>
-        <Breadcrumb successDark >Home/product</Breadcrumb><br/>
-        <Breadcrumb successDarker >Home/product</Breadcrumb>
-    </ThemeProvider>
-)
+export const primary = () => (
+  <ThemeProvider theme={theme}>
+    <Breadcrumb primaryLight>
+      <P secondaryLightColor small>
+        Home
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P secondaryLightColor small>
+        Product
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P xSmall>Headphones</P>
+    </Breadcrumb>
+    <br />
+    <Breadcrumb primary>
+      <P secondaryLightColor small>
+        Home
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P secondaryLightColor small>
+        Product
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P xSmall>Headphones</P>
+    </Breadcrumb>
+    <br />
+    <Breadcrumb primaryDark>
+      <P secondaryLightColor small>
+        Home
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P secondaryLightColor small>
+        Product
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P xSmall>Headphones</P>
+    </Breadcrumb>
+    <br />
+    <Breadcrumb primaryDarker>
+      <P secondaryLightColor small>
+        Home
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P secondaryLightColor small>
+        Product
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P secondaryLightColor xSmall>
+        Headphones
+      </P>
+    </Breadcrumb>
+  </ThemeProvider>
+);
+export const danger = () => (
+  <ThemeProvider theme={theme}>
+    <Breadcrumb danger>
+      <P secondaryLightColor small>
+        Home
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P secondaryLightColor small>
+        Product
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P xSmall>Headphones</P>
+    </Breadcrumb>
+    <br />
+    <Breadcrumb dangerDark>
+      <P secondaryLightColor small>
+        Home
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P secondaryLightColor small>
+        Product
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P xSmall>Headphones</P>
+    </Breadcrumb>
+    <br />
+    <Breadcrumb dangerDarker>
+      <P secondaryLightColor small>
+        Home
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P secondaryLightColor small>
+        Product
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P xSmall>Headphones</P>
+    </Breadcrumb>
+  </ThemeProvider>
+);
+export const secondary = () => (
+  <ThemeProvider theme={theme}>
+    <Breadcrumb secondary>
+      <P small>Home</P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P small>Product</P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P xSmall>Headphones</P>
+    </Breadcrumb>
+    <br />
+    <Breadcrumb secondaryDark>
+      <P small>Home</P>
+      <P medium>/</P>
+      <P small>Product</P> <P medium>/</P> <P xSmall>Headphones</P>
+    </Breadcrumb>
+    <br />
+    <Breadcrumb secondaryDarker>
+      <P secondaryLightColor small>
+        Home
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P secondaryLightColor small>
+        Product
+      </P>
+      <P secondaryDarkColor medium>
+        /
+      </P>
+      <P secondaryLightColor xSmall>
+        Headphones
+      </P>
+    </Breadcrumb>
+  </ThemeProvider>
+);
+export const warning = () => (
+  <ThemeProvider theme={theme}>
+    <Breadcrumb warning>
+      <P small>Home</P>
+      <P medium>/</P>
+      <P small>Product</P> <P medium>/</P> <P xSmall>Headphones</P>
+    </Breadcrumb>
+    <br />
+    <Breadcrumb warningDark>
+      <P small>Home</P>
+      <P medium>/</P>
+      <P small>Product</P> <P medium>/</P> <P xSmall>Headphones</P>
+    </Breadcrumb>
+    <br />
+    <Breadcrumb warningDarker>
+      <P small>Home</P>
+      <P medium>/</P>
+      <P small>Product</P> <P medium>/</P> <P xSmall>Headphones</P>
+    </Breadcrumb>
+  </ThemeProvider>
+);
+export const success = () => (
+  <ThemeProvider theme={theme}>
+    <Breadcrumb success>
+      <P small>Home</P>
+      <P medium>/</P>
+      <P small>Product</P> <P medium>/</P> <P xSmall>Headphones</P>
+    </Breadcrumb>
+    <br />
+    <Breadcrumb successDark>
+      <P small>Home</P>
+      <P medium>/</P>
+      <P small>Product</P> <P medium>/</P> <P xSmall>Headphones</P>
+    </Breadcrumb>
+    <br />
+    <Breadcrumb successDarker>
+      <P small>Home</P>
+      <P medium>/</P>
+      <P small>Product</P> <P medium>/</P> <P xSmall>Headphones</P>
+    </Breadcrumb>
+  </ThemeProvider>
+);
 export default {
-    title: "Breadcrumb",
-    decorators: [withKnobs]
-  };
-  
+  title: "Breadcrumb",
+  decorators: [withKnobs]
+};
