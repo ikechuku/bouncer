3b833ef08b06a6f7b70a9812073a46080f104c1e
Adams Temidire A <gr8temi@gmail.com>
2019-12-06 04:46:36 +0100

    create navbar component

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,14 +1,38 @@
-import React from 'react'
-import Dropdown from './dropdown'
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
+import React from "react";
+// import Dropdown from './dropdown'
+import { BrowserRouter as Router } from "react-router-dom";
+import Text from "../text";
+import { Links, Store, StoreDropdown } from "./styles";
+import Dropdown from "../dropdown";
 
-
-function Navbar(){
-    let navigations = ["IPHONE","IPAD","MACBOOK","ACCESSORIES"]
-    return (
-     <>
-     </>
-    )
+function Navbar() {
+  let navigations = ["IPHONE", "IPAD", "MACBOOK", "ACCESSORIES"];
+  return (
+    <div className="d-flex align-items-center flex-column">
+      <Text dangerDarkerColor bold>
+        BOUNCER
+      </Text>
+      <div className="d-flex justify-content-center flex-wrap">
+        <Router>
+          <Links primaryColor to="/">
+            HOME
+          </Links>
+          <Links primaryColor to="/">
+            <Store>
+              STORE
+              <StoreDropdown>
+                <Dropdown />
+              </StoreDropdown>
+            </Store>
+          </Links>
+          {navigations.map((element, key) => (
+            <Links primaryColor key={key} to={`/${element}`}>
+              {element}
+            </Links>
+          ))}
+        </Router>
+      </div>
+    </div>
+  );
 }
 export default Navbar;
---
+++ src/components/navbar/styles.js
@@ -0,0 +1,34 @@
+import Styled from 'styled-components'
+import { color } from '../color/mixins'
+import { Link} from 'react-router-dom'
+export const Links = Styled(Link)`
+    /* ${color} */
+    text-decoration:none;
+    padding:1rem;
+    &:hover{
+        ${color};
+    }
+    &:focus{
+        ${color};
+    }
+`
+
+export const StoreDropdown = Styled.li`
+    position:absolute;
+    top:20px;
+    visibility: hidden;
+    opacity: 0;
+    transition: 300ms ease-in-out;
+    left:-15rem;
+`
+
+export const Store = Styled.li`
+position:relative;
+list-style:none;
+&:hover{
+    ${StoreDropdown}{
+        visibility:visible;
+        opacity:1;
+    }
+}
+`;
