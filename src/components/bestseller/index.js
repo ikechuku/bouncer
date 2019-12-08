011995a2fdafa3be81f2135299d7340babbb1dcb
ugwumadu116 <ugwumadu116@gmail.com>
2019-12-08 15:54:42 +0100

    added products through states

--- src/components/bestseller/index.js
+++ src/components/bestseller/index.js
@@ -1,28 +1,28 @@
 import React, {useState} from "react";
 import Text from "../text";
-import { ProductsTypes, Products } from "./assets/data/products";
 import { BestSellerContainer } from "./styles";
 import BlockThumbNail from "../thumbnails/BlockThumbNail";
 
-const BestSellers = () => {
-    const [bestSellerLink, setBestSellersLink] = useState(ProductsTypes);
-    const [bestProducts, setBestProducts] = useState(Products);
-    const [view, setView] = useState(8);
+const BestSellers = ({ ProductsTypes, Products }) => {
+  const [bestSellerLink, setBestSellersLink] = useState(ProductsTypes);
+  const [bestProducts, setBestProducts] = useState(Products);
+  const [view, setView] = useState(8);
 
-    const handleFilter = (value) => {
-        const newProducts =
-          value === "All"
-            ? Products
-            : Products.filter(item => item.type === value);
-        setBestProducts(newProducts);
+  const handleFilter = value => {
+    const newProducts =
+      value === "All" ? Products : Products.filter(item => item.type === value);
+    setBestProducts(newProducts);
 
-        const activeProduct = bestSellerLink.map(item => ({ ...item, active: false }));
-        activeProduct.find(item => item.name === value).active = true;
-        setBestSellersLink(activeProduct);
-    }
-    const handleViews = () => {
-        setView(view+ 8)
-    }
+    const activeProduct = bestSellerLink.map(item => ({
+      ...item,
+      active: false
+    }));
+    activeProduct.find(item => item.name === value).active = true;
+    setBestSellersLink(activeProduct);
+  };
+  const handleViews = () => {
+    setView(view + 8);
+  };
   return (
     <BestSellerContainer>
       <div className="d-flex-center">
@@ -60,13 +60,61 @@ const BestSellers = () => {
           )}
         </div>
         {bestProducts.length > view && (
-          <Text onClick={handleViews} className="active loadMore" primaryColor bold ltSpacing0 xSmall>
+          <Text
+            onClick={handleViews}
+            className="active loadMore"
+            primaryColor
+            bold
+            ltSpacing0
+            xSmall
+          >
             Load More..
           </Text>
         )}
       </div>
     </BestSellerContainer>
   );
+};
+
+BestSellers.defaultProps = {
+  ProductsTypes: [
+    {
+      name: "All",
+      active: true
+    },
+    {
+      name: "Mac",
+      active: false
+    },
+    {
+      name: "iPhone",
+      active: false
+    },
+    {
+      name: "iPad",
+      active: false
+    },
+    {
+      name: "Accessories",
+      active: false
+    }
+  ],
+  Products: [
+    {
+      id: 1,
+      name: "Apple Macbook Pro",
+      type: "Mac",
+      ratings: 5,
+      reviews: 9,
+      description:
+        "Nunc facilisis sagittis ullamcorper. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lectus lorem nunc leifend laorevtr istique et congue. Vivamus adipiscin vulputate g nisl ut dolor",
+      price: "499",
+      trending: true,
+      discountedPrice: "599",
+      photo:
+        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp13touch-silver-select-201807?wid=892&hei=820&&qlt=80&.v=1529520056377"
+    }
+  ]
 };
 
 export default BestSellers;
--- stories/BestSellers.stories.js
+++ stories/BestSellers.stories.js
@@ -2,12 +2,17 @@ import React from "react";
 import { withKnobs } from "@storybook/addon-knobs";
 import { ThemeProvider } from "styled-components";
 import theme from "../src/components/theme";
+import {
+  ProductsTypes,
+  Products
+} from "../src/components/bestseller/assets/data/products";
+
 import BestSellers from "../src/components/bestseller";
 
 export const bestSellers = () => {
          return (
            <ThemeProvider theme={theme}>
-             <BestSellers />
+             <BestSellers ProductsTypes={ProductsTypes} Products={Products} />
            </ThemeProvider>
          );
        };
