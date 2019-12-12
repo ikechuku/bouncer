4644b0bacd46d4c7329e93df6658cd36750c6cfd
ikechuku <rockstarik2@gmail.com>
2019-12-12 19:49:13 +0100

    Navbar for the authentication pages

---
+++ src/components/authentication/AuthenticationNav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Logo } from "./styles";
+
+
+const NavLogo = () => {
+  return (
+      <Logo>
+          <img
+    src="https://res.cloudinary.com/decagonbouncer/image/upload/v1575976806/bouncer-frontend/logo_vcjh7n.png"
+    alt="logo"
+  />
+      </Logo>
+  )
+};
+
+export default NavLogo;
