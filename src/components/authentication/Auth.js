8b4f5679df5e2ac97abcdd4e1c80bb82cf8e2d7a
ikechuku <rockstarik2@gmail.com>
2019-12-12 19:48:49 +0100

    Create component to toggle register and login

---
+++ src/components/authentication/Auth.js
@@ -0,0 +1,55 @@
+import React, { Suspense, lazy } from "react";
+import { Route, Switch } from "react-router-dom";
+import { LoginContainer, Navigation } from "./styles";
+import LoginImage from "./assets/loginImage.png";
+
+const Login = lazy(() => import("./Login"));
+const Register = lazy(() => import("./Register"));
+
+const SignIn = () => {
+  return (
+    <LoginContainer>
+      <div className="loginImageContainer">
+        <img src={LoginImage} alt="Login" />
+      </div>
+      <div className="loginForm">
+        <div className="header">
+          <div>
+            <Navigation
+              activeClassName="active"
+              className="boldLink pb-2"
+              to="/login"
+            >
+              Sign in
+            </Navigation>
+          </div>
+
+          <div className="kk">
+            <Navigation
+              activeClassName="active"
+              to="/register"
+              className=" boldLink"
+            >
+              Register
+            </Navigation>
+          </div>
+        </div>
+        <div>
+          <div className="form">
+            <Suspense fallback="loading...">
+              <Switch>
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+              </Switch>
+            </Suspense>
+          </div>
+          <div className="footer">
+            <Navigation to="/forgot-password">Forgot password</Navigation>
+          </div>
+        </div>
+      </div>
+    </LoginContainer>
+  );
+};
+
+export default SignIn;
