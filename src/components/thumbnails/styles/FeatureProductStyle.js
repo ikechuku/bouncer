0d13f923b536ef9e483eabe67020fc1aeca83ed0
ugwumadu116 <ugwumadu116@gmail.com>
2019-12-13 08:59:43 +0100

    implement feature products

---
+++ src/components/thumbnails/styles/FeatureProductStyle.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+// import backgroundColor, { color } from "../../color/mixins";
+
+export const CardWrapperDIV = styled.div`
+         display: flex;
+         width: 21.4375rem;
+         height: 9.875rem;
+         justify-content: space-between;
+         align-items: flex-start;
+         margin-bottom: 1.875rem;
+         & .imageContainer {
+           width: 13.125rem;
+           height: 9.875rem;
+           margin-right: 1.25rem;
+           border: solid 3px #f6f7f8;
+
+           img {
+             width: 100%;
+             height: 100%;
+           }
+         }
+         & .footer {
+           width: 40%;
+           text-align: left;
+           word-wrap: break-word;
+           font-size: 14px;
+           display: flex;
+           align-items: left;
+           justify-content: space-between;
+           flex-direction: column;
+           span {
+             margin: 0.875rem 0;
+           }
+         }
+       `;
+
+
+
