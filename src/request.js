2c822dfe126fc970533e453d309739caad95083e
ugwumadu116 <ugwumadu116@gmail.com>
2019-12-13 14:15:14 +0100

    update request url

--- src/request.js
+++ src/request.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = process.env.REACT_APP_BACKEND_BASE_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const instance = axios.create({
   baseURL: BASE_URL
