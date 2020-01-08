6781f2f45e0ff56f855cd6c5bb59326efa829b2d
Olatunde Sodiq <olatundesodiq@gmail.com>
2020-01-08 15:14:21 +0100

    verify email implementation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import customerReducer from "./customer.reducer";
 import vendorReducer from "./vendor.reducer";
 
 import login from "./Login.reducer";
+import emailVerifification from "./emailVerification.reducer";
 
 export default combineReducers({
   customer: customerReducer,
   vendor: vendorReducer,
-  login
+  login,
+  emailVerifification
 });
