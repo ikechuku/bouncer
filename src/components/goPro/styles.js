250ddad79147f7095995eed6e83fd9f5ce18f872
Olatunde Sodiq <olatundesodiq@gmail.com>
2020-01-15 10:23:08 +0100

    center gopro element

--- src/components/goPro/styles.js
+++ src/components/goPro/styles.js
@@ -4,7 +4,6 @@ import {secondaryDark} from '../color/color'
 
 const GoProWrapper = Styled.div`
     margin: 0 auto;
-    text-align: center;
     background-color:${secondaryDark};
     padding: 5px 20px;
 `
